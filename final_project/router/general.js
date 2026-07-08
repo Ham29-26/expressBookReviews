9986268c6e5fc318cda1fc73bb661bac1826dcb1
Hamza Kazi <ham29.kz@gmail.com>
2026-07-08 10:03:52 -0400

    completed part of project

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,20 +12,27 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
+    res.send(JSON.stringify(books,null,4));
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
+    const isbn = req.params.isbn;
+    if (books[isbn]) {
+        res.send(books[isbn]);
+    } else {
+        res.send("Book with given isbn could not be found")
+    }
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
+    const author = req.params.author;
+    let keys = [];
+    
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get all books based on title
